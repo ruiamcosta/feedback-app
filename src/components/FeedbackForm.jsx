50e88dddfff619a381a09511e8165b84575d208f
Rui Costa <ram666.rc@gmail.com>
2022-03-29 00:07:46 +0100

    Add input validation of FeedbackForm

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from './shared/Button';
 
 const FeedbackForm = () => {
 	const [text, setText] = useState('');
+	const [btnDisabled, setBtnDisabled] = useState(true);
+	const [message, setMessage] = useState('');
 
 	const handleTextChange = e => {
+		if (text === '') {
+			setBtnDisabled(true);
+			setMessage(null);
+		} else if (text !== '' && text.trim().length <= 10) {
+			setBtnDisabled(true);
+			setMessage('Text must be at least 10 charecters');
+		} else {
+			setBtnDisabled(false);
+			setMessage(null);
+		}
 		setText(e.target.value);
 	};
 
@@ -22,8 +34,12 @@ const FeedbackForm = () => {
 						value={text}
 						onChange={handleTextChange}
 					/>
-					<Button type='submit'>Send</Button>
+					<Button type='submit' isDisabled={btnDisabled}>
+						Send
+					</Button>
 				</div>
+
+				{message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	);
